a8478c43b1363508cf9fe3e7e79a42d03871cbba
Rahul Babu <60734994+rahulbabu7@users.noreply.github.com>
2022-05-16 22:33:11 +0530

    Add files via upload

---
+++ swaptwonums.py
@@ -0,0 +1,13 @@
+#getting informations
+a = input("Enter the first number:"); 
+b = input("Enter the second number:"); 
+
+#swapping
+temp = int(a); 
+a = int(b); 
+b = temp; 
+
+#printing the results after swapping
+print("......number after swapping......")
+print("first number = "+str(a)); 
+print("second number = "+str(b)); 
