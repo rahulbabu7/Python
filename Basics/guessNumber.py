b68d13616537b134d7685848228205796cfa3d5a
rahulbabu7 <rahulbabu436@gmail.com>
2023-06-22 21:26:27 +0530

    number guessing game

--- Basics/guessNumber.py
+++ Basics/guessNumber.py
@@ -1,7 +1,17 @@
 import random
+smaller = int(input("Enter the smaller number: "))
+larger = int(input("Enter the larger number: "))
+myNumber = random.randint(smaller, larger)
+count = 0
+while True:
+	count += 1
+	userNumber = int(input("Enter your guess: "))
+	if userNumber < myNumber:
+		print("Too small!")
+	elif userNumber > myNumber:
+		print("Too large!")
+	else:
+		print("Congratulations! You've got it in", count,"tries!")
+		break
 
-lowerLimit = int(input("Enter the lower limit : "))
-higherLimit = int(input("Enter the higher limit : "))
-
-while(1):
     #randomNumber
