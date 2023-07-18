9e65e6e50796c861e48158031e5dfb6b2b01c3d4
rahulbabu7 <rahulbabu436@gmail.com>
2023-07-18 14:34:49 +0530

    added some min and maz

---
+++ numpy/df.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+vale = ['1','2','3']
+df = pd.DataFrame(vale,index=[1,2,3])
+print(df)
--- pandas/df2Graph.py
+++ pandas/df2Graph.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("topper.csv",index_col=1)
+data = pd.read_csv("topper.csv")
 print(data)
+sorted = data.sort_values('cgpa',ascending=True)
+print(sorted)
+maz=data['cgpa'].max()
+print(maz)
+maxCgpa = data[['name','RegNo']][data.cgpa==maz]
+print(maxCgpa) 
 
-plt.title("mapping")
-plt.xlabel("regno")
-plt.ylabel("cgpa")
-plt.scatter(data['RegNo'],data['cgpa'])
+# plt.title("mapping")
+# plt.xlabel("regno")
+# plt.ylabel("cgpa")
+# plt.scatter(data['RegNo'],data['cgpa'])
 
-plt.show()
+# plt.show()
