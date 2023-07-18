f60906e0eb9768cc0fb1bc4404d8459ea4ba4e5a
rahulbabu7 <rahulbabu436@gmail.com>
2023-07-18 21:33:36 +0530

    cipher

--- mod2programs/cipher.py
+++ mod2programs/cipher.py
@@ -7,13 +7,13 @@ for i in lowered:
     
     en = ord(i) +dis
     if(en>ord('z')):
-        en = ord('a')+dis- ((ord('z')-ord(i))+1)
+        en = ord('a')+dis- ((ord('z')-ord(i)+1))
     encry=encry+chr(en)
 print(encry)
 
 for i in encry:
     de= ord(i)-dis
     if(de<ord('a')):
-        de = ord('z') -dis+ ((ord(i)-ord('a'))-1)
+        de = ord('z') -dis+ ((ord(i)-ord('a')-1))
     decry = decry+chr(de)
 print(decry)
