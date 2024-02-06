b451a231e37ce5cb1d05ae73b5b7e28c02a147d0
rahulbabu7 <rahulbabu436@gmail.com>
2024-02-06 23:39:36 +0530

    added string methods

---
+++ MyPratice/stringMethods.py
@@ -0,0 +1,13 @@
+exampleString = "Python is great!"
+
+print(len(exampleString)); # this is not a methos this is a general function
+print(exampleString.upper()) #this is a method. This function belongs to an object String
+print(exampleString.lower());
+
+
+#finding the first index of the character
+print(exampleString.find('g'));
+
+#replace a string
+
+print(exampleString.replace('great','awesome'))  #case sensitive
